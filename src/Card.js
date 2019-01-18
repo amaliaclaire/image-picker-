cad962c3dca7dd7f96f5c32f9b3ab65a953c1c81
Amalia Claire Fergin <acfergin@msn.com>
2019-01-18 10:14:24 -0800

    added card & icon in middle

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div class="ui card">
+      <div class="content">
+        <div class="header">Photo Comparsion</div>
+        <div class="description">
+         <i className="upload icon"></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ImageUpload from './ImageUpload';
+import Card from './Card'
 import './index.css'
 
 const App = () => {
   return(
     <div>
       <div className="title">Deep Cell Image Rendering Prototype</div>
-
       <ImageUpload />
+      <Card />
 
     </div>
   )
